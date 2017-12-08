18c4aa541ce8c6fc42f7e110b0639660853f9f45
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-08 19:09:14 +0530

    Update main.cpp

--- module3/solution1/main.cpp
+++ module3/solution1/main.cpp
@@ -1,20 +1,21 @@
 #include "employees.h"
 #include "company.h"
-#include "queueEmp.h"
+#include "empQueue.h"
+
 #include<iostream>
 
 using namespace std;
 void main()
 {
 	company mt;
+//	empQueue q;
 	
 	int choice=0;
-	
 	while (1)
 	{
 		cout << "\n\n1.create new employee\n2.Display names of employee from a particular year of joining\n3.details of the employee from a particular department having the highest pay";
 		cout << "\n4.Display all details of all employees in ascending order\n5.Update pay by employee id ";
-		cout << "\n6.Move Employee from project to bench\n7.Move Employee from bench to project";
+		cout << "\n6.Move Employee from project to bench\n7.Move Employee from bench to project\n8.View bench";
 		cout << "\nEnter choice ";
 		choice = mt.validation();
 		
@@ -37,10 +38,13 @@ void main()
 			mt.updatePay();
 			break;
 		case 6:
-			mt.enqueue();
+			mt.push();
 			break;
 		case 7:
-			mt.dequeue();
+			mt.pop();
+			break;
+		case 8:
+			mt.viewBench();
 			break;
 		case 0:
 			exit(0);
