50caa012ac7283bc1cefc25a87b1c66ed05baa67
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-11 18:21:29 +0530

    Update Queue.h

--- module3/solution1/Queue.h
+++ module3/solution1/Queue.h
@@ -1,21 +1,26 @@
+#ifndef empQueue_HEADER
+#define empQueue_HEADER
 #pragma once
-#include "employees.h"
-#include "company.h"
+#include<vector>
+#include<iostream>
 
 using namespace std;
 class empQueue
 {
 public:
-	company mt;
-	employees emp;
-	int front;
-	int rear;
-	vector<int> bench;
-	void projectToBench();
-	void benchToProject();
-	void viewBench();
-	int binarySearch(int temp_id);
-	empQueue();
+	
+	empQueue();	
 	~empQueue();
-
+	int front=0;														//variable to store first element in queue
+	int rear=0;															//variable to store last element  in queue
+	vector<string> employee_id;											//variable to store employee id
+	vector<string> department;											//variable to store department
+	vector<string> project_name;										//variable to store project name
+	vector<string> bench;												//variable to store employee id of bench employee
+	void push();
+	void pop();
+	void viewBench();
+	int binarySearch(string temp_id);
+	void copyInfo(vector<string> id, vector<string> pro, vector<string> dept);
 };
+#endif
