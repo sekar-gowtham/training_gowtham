f9e24def91a88e952528b3ceac2abf3e2ae49de8
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-11 18:25:26 +0530

    Update main.cpp

--- module3/solution1/main.cpp
+++ module3/solution1/main.cpp
@@ -1,30 +1,25 @@
 #include "employees.h"
 #include "company.h"
-#include "empQueue.h"
-
 #include<iostream>
-
 using namespace std;
 void main()
 {
 	company mt;
-//	empQueue q;
 	
 	int choice=0;
 	while (1)
 	{
 		cout << "\n\n1.create new employee\n2.Display names of employee from a particular year of joining\n3.details of the employee from a particular department having the highest pay";
 		cout << "\n4.Display all details of all employees in ascending order\n5.Update pay by employee id ";
-		cout << "\n6.Move Employee from project to bench\n7.Move Employee from bench to project\n8.View bench";
+		cout << "\n6.Move Employee from project to bench\n7.Move Employee from bench to project\n8.View bench\n0.Exit";
 		cout << "\nEnter choice ";
-		choice = mt.validation();
+		choice = mt.validation();											//get the choice from the user
 		
-		switch (choice)
+		switch (choice)														
 		{
 		case 1:
 			mt.createNewEmployee();
 			break;
-		
 		case 2:
 			mt.namesFromDateOfJoining();
 			break;
@@ -38,20 +33,19 @@ void main()
 			mt.updatePay();
 			break;
 		case 6:
-			mt.push();
+			mt.queue.push();
 			break;
 		case 7:
-			mt.pop();
+			mt.queue.pop();
 			break;
 		case 8:
-			mt.viewBench();
+			mt.queue.viewBench();
 			break;
 		case 0:
 			exit(0);
 		default:
 			cout << "\nEnter valid choice ";
 		}
-		
 	}
 	cin.clear();
 	cin.ignore(1000,'\n');
