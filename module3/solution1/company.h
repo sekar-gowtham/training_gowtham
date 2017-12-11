f762a5e4640d51fc3dfa2c6a105668a91dbcb43f
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-11 18:23:40 +0530

    Update company.h

--- module3/solution1/company.h
+++ module3/solution1/company.h
@@ -1,34 +1,30 @@
 #pragma once
 #ifndef company_HEADER
 #define company_HEADER
-#include<vector>
+
 #include<iostream>
 #include "employees.h"
 #include "empQueue.h"
 using namespace std;
 class company
 {
-	
 public:
+	employees employee;
+	empQueue queue;
 	company();
 	~company();
-	employees  employee;
-	
 	int front = 0;
 	int rear = 0;
-
 	void createNewEmployee();
 	void displayDetails();
 	void namesFromDateOfJoining();
 	void employeeWithHighPay();
 	void updatePay();
-	int binarySearch(int temp_id);
+	int binarySearch(string temp_id);
 	int validation(void);
-	void push();
+	/*void push();
 	void pop();
-	void viewBench();
-
-　
+	void viewBench();*/
 };
 
 #endif
