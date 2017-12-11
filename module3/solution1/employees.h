1059a4272c87b2ca113520efb21ba8742b16549c
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-11 18:22:20 +0530

    Update employees.h

--- module3/solution1/employees.h
+++ module3/solution1/employees.h
@@ -8,22 +8,16 @@ using namespace std;
 class employees
 {
 public:
-	//empQueue q;
-	int id = 100;
-
-	vector<int> employee_id;
+	int id = 101;
+	vector<string> employee_id;
 	vector<int> pay;
 	vector<int> year_of_joining;
 	vector<string> employee_name;
 	vector<string> project_name;
 	vector<string> department;
-	vector <int> bench;
-	
-
+	vector<string> bench;
 	employees();
-	
 	~employees();
-
 };
 
 #endif
