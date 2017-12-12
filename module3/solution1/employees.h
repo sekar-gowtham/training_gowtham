5546111954445684145bd0fadd271e57fed3be5b
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-12 15:35:50 +0530

    Update employees.h

--- module3/solution1/employees.h
+++ module3/solution1/employees.h
@@ -9,13 +9,13 @@ class employees
 {
 public:
 	int id = 101;
-	vector<string> employee_id;
-	vector<int> pay;
-	vector<int> year_of_joining;
-	vector<string> employee_name;
-	vector<string> project_name;
-	vector<string> department;
-	vector<string> bench;
+	vector<string> employee_id;													//variable to store employee id
+	vector<float> pay;															//variable to store pay
+	vector<int> year_of_joining;												//variable to store year of joining 
+	vector<string> employee_name;												//variable to store employee name
+	vector<string> project_name;												//variable to store project name
+	vector<string> department;													//variable to store department
+	vector<string> bench;														//variable to store bench employees
 	employees();
 	~employees();
 };
