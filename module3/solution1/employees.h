1309c3d99a09e7977eea5ca50337526fcf5be45e
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-18 16:14:41 +0530

    Update employees.h

--- module3/solution1/employees.h
+++ module3/solution1/employees.h
@@ -4,18 +4,28 @@
 
 #include<vector>
 #include<iostream>
+#include<string>
+
 using namespace std;
 class employees
 {
 public:
-	int id = 101;
-	vector<string> employee_id;													//variable to store employee id
-	vector<float> pay;															//variable to store pay
-	vector<int> year_of_joining;												//variable to store year of joining 
-	vector<string> employee_name;												//variable to store employee name
-	vector<string> project_name;												//variable to store project name
-	vector<string> department;													//variable to store department
-	vector<string> bench;														//variable to store bench employees
+	
+	string employee_id;													//variable to store employee id
+	float pay;															//variable to store pay
+	int year_of_joining;												//variable to store year of joining 
+	string employee_name;												//variable to store employee name
+	string project_name;												//variable to store project name
+	string department;													//variable to store department
+	
+	void setId(string id);
+	void setPay(float tpay);
+	void setYearOfJoining(int yoj);
+	void setEmployeeName(string name);
+	void setProjectName(string project);
+	void setDepartment(string dept);
+
+	
 	employees();
 	~employees();
 };
