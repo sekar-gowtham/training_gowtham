4b3fa65afe9efb7f9931a1a77faff55b20266744
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-18 16:15:10 +0530

    Update main.cpp

--- module3/solution1/main.cpp
+++ module3/solution1/main.cpp
@@ -4,10 +4,16 @@
 using namespace std;
 void main()
 {
+
 	company mt;
-	int choice=1;
+	int choice=0;
 	while (1)
 	{
+		cout << "\n\n1.Add new employee\n2.Display names of employee from a particular year of joining\n3.details of the employee from a particular department having the highest pay";
+		cout << "\n4.Display all details of all employees in ascending order\n5.Update pay by employee id ";
+		cout << "\n6.Move Employee from project to bench\n7.Move Employee from bench to project\n8.View bench\n0.Exit";
+		cout << "\nEnter choice ";
+		choice = mt.getChoice();												//get the choice from the user
 		
 		switch (choice)														//execute case according to user's input
 		{
@@ -27,27 +33,18 @@ void main()
 			mt.updatePay();
 			break;
 		case 6:
-			mt.queue.push();
+			mt.pushEmployee();
 			break;
 		case 7:
-			mt.queue.pop();
+			mt.popEmployee();
 			break;
 		case 8:
-			mt.queue.viewBench();
+			mt.viewBenchEmployee();
 			break;
 		case 0:
 			exit(0);
 		default:
 			cout << "\nEnter valid choice ";
 		}
-		cout << "\n\n1.Add new employee\n2.Display names of employee from a particular year of joining\n3.details of the employee from a particular department having the highest pay";
-		cout << "\n4.Display all details of all employees in ascending order\n5.Update pay by employee id ";
-		cout << "\n6.Move Employee from project to bench\n7.Move Employee from bench to project\n8.View bench\n0.Exit";
-		cout << "\nEnter choice ";
-		choice = mt.getChoice();												//get the choice from the user
-
-	}
-	cin.clear();																//clearing the error flags of cin
-	cin.ignore(1000,'\n');														//ignore the wrong input
-	cin.get();
+	}																
 }
