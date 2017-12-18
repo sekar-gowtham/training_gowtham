2c1f6629fcf4b9d201b265caacb1c84912172ce5
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-18 16:14:04 +0530

    Update employees.cpp

--- module3/solution1/employees.cpp
+++ module3/solution1/employees.cpp
@@ -12,3 +12,28 @@ employees::~employees()
 {
 }
 
+void employees::setId(string id)
+{
+	employee_id = id;
+}
+
+void employees::setEmployeeName(string name)
+{
+	employee_name = name;
+}
+void employees::setDepartment(string dept)
+{
+	department = dept;
+}
+void employees::setProjectName(string project)
+{
+	project_name = project;
+}
+void employees::setYearOfJoining(int yoj)
+{
+	year_of_joining = yoj;
+}
+void employees::setPay(float salary)
+{
+	pay = salary;
+}
