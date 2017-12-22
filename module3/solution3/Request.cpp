49f5a76e2d6372c6d376a519a959507ba5d0d70a
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-22 18:11:21 +0530

    Create Request.cpp

---
+++ module3/solution3/Request.cpp
@@ -0,0 +1,23 @@
+#include "Request.h"
+
+　
+　
+Request::Request()
+{
+}
+
+　
+Request::~Request()
+{
+}
+
+void Request::setRequest(string id,string req)
+{
+
+	air_id = id;
+	request_type = req;
+	
+
+}
+
+　
