e697fe4625ab84cc368fce1cb91a53d6d1293fc1
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-22 18:14:35 +0530

    Create Simulation.cpp

---
+++ module3/solution3/Simulation.cpp
@@ -0,0 +1,25 @@
+#include <thread>
+#include <iostream>
+#include <ctime>
+#include <thread>
+#include <iomanip> 
+#include "Airport.h"
+#include "Airplane.h"
+
+using namespace std;
+
+void main()
+{
+	Airport cbe;
+	string aero_id;
+	
+
+	aero_id=cbe.genarateAirId();
+
+	cbe.airobj.generateRequest();
+
+　
+	cin.get();
+}
+
+　
