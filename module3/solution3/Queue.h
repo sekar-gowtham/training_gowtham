22a4bec0d5bff08edd166ad5c833d97098717300
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2017-12-26 18:46:20 +0530

    Update Queue.h

--- module3/solution3/Queue.h
+++ module3/solution3/Queue.h
@@ -1,5 +1,7 @@
 #include <string>
 #include <vector>
+#include <thread>
+#include <iostream>
 #include "Request.h"
 using namespace std;
 #pragma once
@@ -10,16 +12,19 @@ private:
 	vector<Request> landingQueue;
 	vector<Request> takeoffQueue;
 	
-	int front = 0;														
-	int rear = 0;
+	int landing_count = 0;
+	int takeoff_count = 0;
+	int landing_front = 0;														
+	int landing_rear = 0;
+	int takeoff_front = 0;
+	int takeoff_rear = 0;
 public:
-	void setId(string);
-	string getId();
-
+	void displayQueue();
+	void landingTakeoffCount();
 	void push(string,string);
-	void pop();
-	void display();
-
+	bool pop();
+	
+	void checkQueue();
 	Queue();
 	~Queue();
 };
