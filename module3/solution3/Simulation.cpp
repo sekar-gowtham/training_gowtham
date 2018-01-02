3433dd0a82cbce1e27138e1a0084071d64178814
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2018-01-02 12:16:02 +0530

    Update Simulation.cpp

--- module3/solution3/Simulation.cpp
+++ module3/solution3/Simulation.cpp
@@ -10,7 +10,7 @@ void main()
 	time_t total_time;
 	total_time = time(NULL)+240;
 	
-	thread randomRequestGenerate(&Airport::generateRequest, &cbe,total_time);
+	thread randomRequestGenerate(&Airport::callGenerateRequest, &cbe,total_time);
 	thread continuosCheckRunway1(&Airport::callCheckRunway1, &cbe,total_time);
 	thread continuosCheckRunway2(&Airport::callCheckRunway2, &cbe,total_time);
 
