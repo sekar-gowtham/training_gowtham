3484a2dafe57900e0f02f80bd317d333aae12c2e
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2018-02-06 14:10:20 +0000

    Create client.cpp

---
+++ sample/client 1/client.cpp
@@ -0,0 +1,49 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <winsock2.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    WSADATA WSAData;
+    SOCKET server;
+    SOCKADDR_IN addr;
+	int i;
+    WSAStartup(MAKEWORD(2,0), &WSAData);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+ 
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(138);
+ 
+    connect(server, (SOCKADDR *)&addr, sizeof(addr));
+    cout << "Connected to server!" << endl;
+	char buffer1[1024];
+	char buffer[1024] = "<hangman><username>computer</username></hangman>";
+	
+
+	
+		send(server, buffer, sizeof(buffer), 0);
+		cout << "Message sent!" << endl;
+		recv(server, buffer1, sizeof(buffer1), 0);
+		cout << "message received " << buffer1 << endl;
+		while (1)
+		{
+			cout << "\nenter ";
+			cin >> buffer;
+			send(server, buffer, sizeof(buffer), 0);
+			cout << "Message sent!" << endl;
+		}
+		
+		
+	
+		
+
+	//closesocket(server);
+    WSACleanup();
+    cout << "Socket closed." << endl << endl;
+	cin.get();
+	cin.ignore(1000, '\n');
+
+}
