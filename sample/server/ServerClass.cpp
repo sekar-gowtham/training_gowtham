97bbfb7e9596ee572cbd1f8083ac42a02a7ad7be
Gowtham <32456355+gowtham7788@users.noreply.github.com>
2018-02-07 19:28:43 +0530

    Add files via upload

--- sample/server/ServerClass.cpp
+++ sample/server/ServerClass.cpp
@@ -10,69 +10,100 @@ ServerClass::ServerClass()
 ServerClass::~ServerClass()
 {
 }
-void ServerClass::receive_message()
+void ServerClass::create_game(SOCKET client)
 {
-	//int Iteration = 3;
-	char UserName[1024];
-	while (1)
-	{
-		recv(client[0], UserName, sizeof(UserName), 0);
-	}
-	cout << UserName;
-	//receive_message();
+	//
+}
+void ServerClass::game_detail(SOCKET client)
+{
+	// get data from DB and send to UI
 
+}
+void ServerClass::receive_message(SOCKET client)
+{
+	char buffer[1024];
+	char buffer1[1024] = "hi how r u";
+	int ReceivedBytes = 0;
+	string TagName;
+	while (ReceivedBytes != -1)
+	{
+		ReceivedBytes = recv(client, buffer, sizeof(buffer), 0);
+		if (ReceivedBytes > 0);
+		{
+			cout << "\nmessage received " << buffer;
+			document.parse<0>(&buffer[0]);
+			xml_node<> *node = document.first_node();
+			xml_node<> *node1 = node->first_node(); 
+			TagName = node1->name();
+			if (TagName == "requestgamedetails")
+			{
+				cout << "\nuser requested game detail";
+				game_detail(client);
+			}
+			else if (TagName == "creategame")
+			{
+				cout << "\nuser send create game ";
+				create_game(client);
+			}
+			else if (TagName == "joingame")
+			{
+				cout << "\nuser send join game ";
+			}
+
+
+
+
+			send(client, buffer1, sizeof(buffer1), 0);
+		}
+	}
 }
 void ServerClass::accept_connection()
 {
-	WSADATA WSAData;
-
-	int Iteration = 0;
-
-	SOCKADDR_IN serverAddr, clientAddr;
-
+	if (WSAStartup(MAKEWORD(2, 0), &WSAData) != 0)
+	{
+		cout << "\nWSA startup faild ";
+	}
 	WSAStartup(MAKEWORD(2, 0), &WSAData);
-	server = socket(AF_INET, SOCK_STREAM, 0);
 
+	if ((server = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+	{
+		cout << "\nCould not create socket ";
+	}
 	serverAddr.sin_addr.s_addr = INADDR_ANY;
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(138);
-
-	bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr));
+	if ((::bind(server, (SOCKADDR *)&serverAddr, sizeof(serverAddr))) < 0)
+	{
+		cout << "\nbind error ";
+	}
 	listen(server, 3);
-
-	cout << "Listening for incoming connections..." << endl;
-	char buffer1[1024] = "hi from server";
-	char UserName[1024];
+	cout << "\nListening for incoming connections..." << endl;
+	char buffer[1024];
+	char buffer1[1024] = { "hi how r u" };
 	int clientAddrSize = sizeof(clientAddr);
 	while (1)
 	{
-	
-		if ((client[Iteration] = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
+		if ((client[ClientCount] = accept(server, (SOCKADDR *)&clientAddr, &clientAddrSize)) != INVALID_SOCKET)
 		{
-			//receive_message();
-			cout << "\nClient connected!" << endl;
-			//for (int i = 0; i < Iteration; i++)
-			//{
-			send(client[Iteration], buffer1, sizeof(buffer1), 0);
-				
-			//}
-		//thread call(&receive_message);
-				Iteration++;
-			
+			cout << "Client connected!" << endl;
+			receive[ClientCount] = thread(&ServerClass::receive_message,this, client[ClientCount]);
+			memset(buffer, 0, sizeof(buffer));
+			ClientCount++;
 		}
 		else
 		{
-			cout << "Client Not connected!" << endl;
+			cout << "Client not connected!" << endl;
 		}
 	}
-	//store the UserName in DB
-	
-
+	cout << "Client disconnected." << endl;
+	for (int j = 0; j < ClientCount; j++)
+	{
+		receive[j].join();
+	}
 }
 
-
-void ServerClass::send_message()
-{
-	
-}
-
+//void ServerClass::send_message()
+//{
+//	
+//}
+//
--- sample/server/ServerClass.h
+++ sample/server/ServerClass.h
@@ -1,17 +1,27 @@
 #pragma once
+#include "rapidxml.hpp"
+#include "rapidxml_print.hpp"
 #include <iostream>
 #include <thread>
 #include <winsock2.h>
+#pragma comment(lib,"libws2_32.a")
 using namespace std;
+using namespace rapidxml;
 class ServerClass
 {
 private:
+	int ClientCount = 0;
 	SOCKET server, client[7];
+	WSADATA WSAData;
+	SOCKADDR_IN serverAddr, clientAddr;
+	thread receive[4];
+
 public:
+	xml_document<> document;
 	ServerClass();
 	~ServerClass();
 	void accept_connection();
 	void send_message();
-	void receive_message();
+	void receive_message(SOCKET client);
 };
 
